8a4ad56e607af722fde733adb1091462af3e5957
maimanchuang <91313307@qq.com>
2022-10-22 13:26:36 +0800

    初次添加

---
+++ springCloudParent/src/test/java/com/example/springcloudparent/SpringCloudParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
