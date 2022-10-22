55cb7118a7a115910aab77871e24e3f48f27c520
maimanchuang <91313307@qq.com>
2022-10-22 14:10:53 +0800

    新建client

---
+++ spring_config_client/src/main/java/com/example/spring_config_client/SpringConfigClientApplication.java
@@ -0,0 +1,15 @@
+package com.example.spring_config_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+@SpringBootApplication
+@EnableEurekaClient
+public class SpringConfigClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringConfigClientApplication.class, args);
+    }
+
+}
