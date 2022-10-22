403cb900705245db0d01303888087e977869da84
maimanchuang <91313307@qq.com>
2022-10-22 18:23:22 +0800

    保留版本

--- spring_config_service/src/main/java/com/example/spring_config_service/SpringConfigServiceApplication.java
+++ spring_config_service/src/main/java/com/example/spring_config_service/SpringConfigServiceApplication.java
@@ -1,11 +1,15 @@
 package com.example.spring_config_service;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableConfigServer
 public class SpringConfigServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringConfigServiceApplication.class, args);
